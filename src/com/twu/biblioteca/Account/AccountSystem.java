69e80727c90ae1b8095c9be03ab4bca102404b8d
slizeng <251231296@qq.com>
2016-02-02 02:09:54 +0800

    add account system test

--- src/com/twu/biblioteca/Account/AccountSystem.java
+++ src/com/twu/biblioteca/Account/AccountSystem.java
@@ -1,17 +1,28 @@
 package Account;
 
+import Console.Console;
+
 import java.util.HashMap;
 
 public class AccountSystem {
+    private Console console;
     HashMap <String, BaseAccount> accountHashMap;
 
-    public AccountSystem() {
+    public AccountSystem(Console console) {
+        this.console = console;
         accountHashMap = new HashMap<String, BaseAccount>();
         accountHashMap.put("admin", new BaseAccount("admin", "admin", "admin", "admin"));
     }
 
-    public void register(String name, String email, String phoneNumber, String password) {
-        accountHashMap.put(email, new BaseAccount(name, email, phoneNumber, password));
+    public boolean register(String name, String email, String phoneNumber, String password) {
+        if (accountHashMap.containsKey(email)){
+            console.print("Register failed.\n");
+            return false;
+        }else {
+            accountHashMap.put(email, new BaseAccount(name, email, phoneNumber, password));
+            console.print("Register successfully.\n");
+            return true;
+        }
     }
 
     public BaseAccount checkAccount(String email) {
--- test/com/twu/biblioteca/Account/AccountSystemTest.java
+++ test/com/twu/biblioteca/Account/AccountSystemTest.java
@@ -1,19 +1,46 @@
 package Account;
 
+import Console.Console;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
 
 public class AccountSystemTest {
+    private Console console;
+    private AccountSystem accountSystem;
+
+    @Before
+    public void setUp() throws Exception {
+        console = mock (Console.class);
+        accountSystem = new AccountSystem(console);
+    }
 
     @Test
-    public void testRegister() throws Exception {
+    public void testRegisterSuccessfully() throws Exception {
+        AccountSystem accountSystem = new AccountSystem(console);
+        boolean flag = accountSystem .register("Sli", "sli616@163.com", "15884373717", "123456");
 
+        assertTrue(flag);
+        verify(console, times(1)).print("Register successfully.\n");
+    }
+
+
+    @Test
+    public void testRegisterFailed() throws Exception {
+        AccountSystem accountSystem = new AccountSystem(console);
+        boolean flag = accountSystem .register("admin", "admin", "admin", "admin");
+
+        assertFalse(flag);
+        verify(console, times(1)).print("Register failed.\n");
     }
 
     @Test
     public void testCheckAccount() throws Exception {
-        AccountSystem accountSystem = new AccountSystem();
+
         accountSystem.register("Sli", "sli616@163.com", "15884373717", "123456");
         BaseAccount targetAccount = accountSystem.checkAccount("sli616@163.com");
 
