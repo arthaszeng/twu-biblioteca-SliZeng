f1e6c8bdd82c3e71390bc4014287704f28cf8d1c
slizeng <251231296@qq.com>
2016-02-02 01:45:54 +0800

    add account system test

--- src/com/twu/biblioteca/Account/AccountSystem.java
+++ src/com/twu/biblioteca/Account/AccountSystem.java
@@ -10,16 +10,16 @@ public class AccountSystem {
         accountHashMap.put("admin", new BaseAccount("admin", "admin", "admin", "admin"));
     }
 
-    public BaseAccount register(String name, String email, String phoneNumber, String password) {
-        return new BaseAccount(name, email, phoneNumber, password);
+    public void register(String name, String email, String phoneNumber, String password) {
+        accountHashMap.put(email, new BaseAccount(name, email, phoneNumber, password));
     }
 
-    public String checkAccount(String email) {
+    public BaseAccount checkAccount(String email) {
         BaseAccount targetAccount = accountHashMap.get(email);
         if (targetAccount == null){
             return null;
         }else {
-            return targetAccount.getRole();
+            return targetAccount;
         }
     }
 }
--- test/com/twu/biblioteca/Account/AccountSystemTest.java
+++ test/com/twu/biblioteca/Account/AccountSystemTest.java
@@ -5,12 +5,22 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class AccountSystemTest {
-    @Test
-    public void testLoginForUser() throws Exception {
-        AccountSystem accountSystem = new AccountSystem();
-        BaseAccount baseAccount = accountSystem.register("Sli", "sli616@163.com", "15884373717", "123456");
-        String role = accountSystem.checkAccount("sli616@163.com");
 
-        assertEquals("user", role);
+    @Test
+    public void testRegister() throws Exception {
+
+    }
+
+    @Test
+    public void testCheckAccount() throws Exception {
+        AccountSystem accountSystem = new AccountSystem();
+        accountSystem.register("Sli", "sli616@163.com", "15884373717", "123456");
+        BaseAccount targetAccount = accountSystem.checkAccount("sli616@163.com");
+
+        assertEquals("sli616@163.com", targetAccount.getEmail());
+        assertEquals("Sli", targetAccount.getName());
+        assertEquals("123456", targetAccount.getPassword());
+        assertEquals("15884373717", targetAccount.getPhoneNumber());
+        assertEquals("user", targetAccount.getRole());
     }
 }
