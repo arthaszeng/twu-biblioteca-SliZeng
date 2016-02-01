e5ed9b8699c8823b267b4255c73063296675b038
slizeng <251231296@qq.com>
2016-02-02 03:37:16 +0800

    adding permissions of check-out and return.

---
+++ src/com/twu/biblioteca/Menu/Option/ReturnOption.java
@@ -0,0 +1,21 @@
+package Menu.Option;
+
+import Biblioteca.BibliotecaApp;
+import Console.Console;
+
+public class ReturnOption extends Option{
+    private Console console;
+    public ReturnOption() {
+        this.key = "R";
+        this.name = "Return books or movies";
+        console = new Console();
+    }
+
+    @Override
+    public boolean operate(BibliotecaApp bibliotecaApp) {
+        if (bibliotecaApp.isLogined()) {
+            bibliotecaApp.returnBook(console.scaner());
+            return true;
+        }else return false;
+    }
+}
