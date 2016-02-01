7138006c2ddac73fe44a6e7c9c03ef657157885b
slizeng <251231296@qq.com>
2016-02-02 00:48:52 +0800

    refactor

--- test/com/twu/biblioteca/BibliotecaApp/MovieModule.java
+++ test/com/twu/biblioteca/BibliotecaApp/MovieModule.java
@@ -6,8 +6,9 @@ import Menu.Menu.MainMenu;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.*;
 
 public class MovieModule {
 
@@ -36,4 +37,35 @@ public class MovieModule {
         verify(console).print("<<3 idiots>>\t2009\tRajkumar Hirani\t9.1\ttt1187043\n");
         verify(console).print("<<Peekay>>\t2014\tRajkumar Hirani\t8.3\ttt2338151\n");
     }
+
+    @Test
+    public void testCheckoutMovieSuccessful() throws Exception {
+        Boolean flag1 = bibliotecaApp.checkout("tt1187043");
+        Boolean flag2 = bibliotecaApp.checkout("tt2338151");
+
+        bibliotecaApp.showAllMovies();
+
+        assertTrue(flag1);
+        assertTrue(flag2);
+        verify(console, times(2)).print("Thank you! Enjoy the movie.\n");
+        verify(console, times(0)).print("3 idiots\n");
+        verify(console, times(0)).print("Peekay\n");
+    }
+
+    @Test
+    public void testCheckoutMovieFailed() throws Exception {
+        Boolean flag1 = bibliotecaApp.checkout("wrong");
+        Boolean flag2 = bibliotecaApp.checkout("98794");
+
+        bibliotecaApp.showAllMovies();
+
+        assertFalse(flag1);
+        assertFalse(flag2);
+        verify(console, times(0)).print("Thank you! Enjoy the movie.\n");
+        verify(console, times(2)).print("That is not available\n");
+        verify(console, times(1)).print("3 idiots\n");
+        verify(console, times(1)).print("Peekay\n");
+    }
+
+
 }
